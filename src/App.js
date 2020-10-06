54f784f999d30fae3801376e2193ae117ba88cca
AmyNeko <nichaporn.kalantanon@gmail.com>
2020-10-07 02:56:12 +0700

    save and load

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import BlocklyComponent, { Block, Value, Field, Category } from "./Blockly";
 import BlocklyPython from "blockly/python";
-import { babyAiService } from "./services/babyAiService";
+import { babyAiService, exportWorkspace } from "./services/babyAiService";
 import { CodeBlock, dracula } from "react-code-blocks";
 import Blockly from 'blockly'
 
@@ -29,7 +29,7 @@ const ConsoleComponent = ({ data = [] }) => {
 };
 
 //Top bar componenet NOTE that save and load is in developed
-const NavbarComponent = ({ generateCode, toggle, save, load}) => {
+const NavbarComponent = ({ generateCode, toggle, save, load, input}) => {
   return (
     <div
       style={{
@@ -58,12 +58,14 @@ const NavbarComponent = ({ generateCode, toggle, save, load}) => {
           Execute
         </button>
         {/* Wait for API Line to save and download xml file */}
-        {/* <button onClick={save} className="saveBtn">
+        <button onClick={save} className="saveBtn">
           Save
         </button>
         <button onClick={load} className="loadBtn">
           Load
-        </button> */}
+        </button>
+        <input type="file" name="file" onChange={input} display="none"/>
+        
       </div>
     </div>
   );
@@ -122,11 +124,12 @@ const WorkspaceComponent = ({ initWorkspaceRef }) => {
 
 //Main application window
 const App = () => {
-  const workspaceRef = useRef(null);
+  var loadedContent = ""
+  const workspaceRef = useRef(null); //workspace ref
+
   const [togglePreviewCode, setTogglePreviewCode] = useState(false);
   const [executeCodeResponse, setExecuteCodeResponse] = useState([]);
   const [codePreview, setCodePreview] = useState("");
-  const [workspaceContent, setWorkspaceContent] = useState("");
 
   const getCode = () => {
     let codeFromBlock = BlocklyPython.workspaceToCode(
@@ -144,24 +147,46 @@ const App = () => {
     var xml = Blockly.Xml.workspaceToDom(workspaceRef.current.workspace);
     var xml_text = Blockly.Xml.domToText(xml);
     // setWorkspaceContent(xml_text);
+    console.log("This is your workspace as text");
+    console.log("Copy the following line, create a new file and save as .txt")
     console.log(xml_text);
+    
+    // const response =  exportWorkspace(xml_text);
+
     }catch(e) {
       console.log(e)
     }
   }
   //load workspace
   const loadWorkspace = () => {
-
+    // const test = '<xml xmlns="https://developers.google.com/blockly/xml"><block type="controls_repeat_ext" id="{]bu2.d];];(p7qjm0tL" x="290" y="96"><value name="TIMES"><block type="math_number" id="?Jdii%THAK4XU=9ex77A"><field name="NUM">10</field></block></value><statement name="DO"><block type="text_print" id="*iL-qlAy/xh%4aB#zPMj"><value name="TEXT"><block type="text" id="Y9ph%[APaN)B$+_grX}7"><field name="TEXT">test</field></block></value></block></statement></block></xml>'
+    // const saved = Blockly.Xml.textToDom(test)
+    // Blockly.Xml.clearWorkspaceAndLoadFromXml(saved, workspaceRef.current.workspace)
+    const XML = Blockly.Xml.textToDom(loadedContent)
+    Blockly.Xml.clearWorkspaceAndLoadFromXml(XML, workspaceRef.current.workspace)
   }
-  //END bug function------
+
+  const onFileChangeHandle = (event) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const text = (e.target.result)
+      loadedContent = text;
+    }
+    reader.readAsText(event.target.files[0])
+  }
+
+  //END test function------
 
   //Generate code from block connection
   const generateCode = async () => {
     const codeFromBlock = getCode();
+    try{
     const response = await babyAiService(codeFromBlock);
     setExecuteCodeResponse(response.data.split("\n"));
+    }catch(e) {
+      alert('Cannot execute a code for reason: ' + e)
+    }
   };
-
 
   //Toggle preview code besied OUTPUT tab
   const onHandlerTogglePreviewCode = () => {
@@ -176,8 +201,9 @@ const App = () => {
       <NavbarComponent
         generateCode={generateCode}
         toggle={onHandlerTogglePreviewCode}
-        // save={saveWorkspace}
-        // load={loadWorkspace}
+        save={saveWorkspace}
+        load={loadWorkspace}
+        input={onFileChangeHandle}
       />
         <WorkspaceComponent
           initWorkspaceRef={(ref) => {
--- src/services/babyAiService.js
+++ src/services/babyAiService.js
@@ -1,16 +1,25 @@
 import axios from "axios";
 
-const BASE_URL = `http://babyai.org:5000/execute`;
+const BASE_URL = `http://babyai.org:5000/`;
 const HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Content-Type": "text/plain",
 };
 
 export const babyAiService = async (data) => {
-  const response = await axios.post(BASE_URL, data, {
+  const response = await axios.post(BASE_URL + 'execute', data, {
     headers: HEADERS,
     withCredentials: true,
     credentials: "same-origin",
   });
   return response;
 };
+
+export const exportWorkspace = async (data) => {
+  const response = await axios.post(BASE_URL + 'workspace/export', data, {
+    headers: HEADERS,
+    withCredentials: true,
+    credentials: "same-origin",
+  });
+  return response;
+}
